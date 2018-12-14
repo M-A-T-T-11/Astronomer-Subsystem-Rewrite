a17aba07fcc355eafa6387f8b5d0e035bf7cb511
Matthew Wang <Matthew@MacBook-Air.local>
2018-12-14 16:21:29 -0500

    Added update telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/subsystemutils/SubsystemManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/subsystemutils/SubsystemManager.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.subsystems.subsystemutils;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.subsystemutils.Subsystem;
 
 public class SubsystemManager {
+
   private Subsystem[] subSystems;
-  public SubsystemManager(Subsystem ...subSystems) {
-    this.subSystems = subSystems;
+  private Telemetry telemetry;
+  public SubsystemManager(Telemetry telemetry, Subsystem ...subSystems) {
+      this.telemetry = telemetry;
+      this.subSystems = subSystems;
   }
 
   public void init() {
@@ -18,5 +22,6 @@ public class SubsystemManager {
     for (Subsystem subSystem : subSystems)  {
       subSystem.update();
     }
+    telemetry.update();
   }
 }
